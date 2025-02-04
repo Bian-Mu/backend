bfb4b6ca19ce92672946ee7374930ad475f713cc
Bian-Mu <2570772184@qq.com>
2025-02-04 21:36:08 +0800

    feat:重构&日记更新

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -12,148 +11,6 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-func main() {
-	router := gin.Default()
-
-	// 启用CORS
-	router.Use(corsMiddleware())
-
-	// 静态文件服务
-	router.Static("/public", "./public")
-	
-	// 启用CORS
-	router.Use(corsMiddleware())
-
-	// API端点：获取public/2025md目录下的指定Markdown文件
-	router.GET("/api/markdown/:filename", func(c *gin.Context) {
-		filename := c.Param("filename")
-		filePath := filepath.Join("public", "2025md", filename)
-		data, err := ioutil.ReadFile(filePath)
-		if err != nil {
-			c.String(http.StatusNotFound, "File not found")
-			return
-		}
-		c.Data(http.StatusOK, "text/plain", data)
-	})
-
-	// API端点：获取public/2025pic目录下的指定图片文件
-	router.GET("/api/image/:filename", func(c *gin.Context) {
-		filename := c.Param("filename")
-		filePath := filepath.Join("public", "2025pic", filename)
-		data, err := ioutil.ReadFile(filePath)
-		if err != nil {
-			c.String(http.StatusNotFound, "File not found")
-			return
-		}
-		c.Data(http.StatusOK, "image/"+filepath.Ext(filename)[1:], data)
-	})
-
-	// API端点：获取歌曲信息
-	router.GET("/api/songInfo", func(c *gin.Context) {
-		songId := c.Query("songId")
-		songUrl := fmt.Sprintf("https://music.163.com/api/v3/song/detail?c=[{\"id\":%s}]", songId)
-
-		body, err := fetch(songUrl)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
-			return
-		}
-		c.Data(http.StatusOK, "application/json", body)
-	})
-
-	// API端点：获取歌词
-	router.GET("/api/lyricsInfo", func(c *gin.Context) {
-		songId := c.Query("songId")
-		lyricsUrl := fmt.Sprintf("https://music.163.com/api/song/media?id=%s", songId)
-
-		body, err := fetch(lyricsUrl)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
-			return
-		}
-		c.Data(http.StatusOK, "application/json", body)
-	})
-
-	// API端点：获取专辑封面
-	router.GET("/api/picInfo", func(c *gin.Context) {
-		url := c.Query("picUrl")
-
-		body, err := fetch(url)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
-			return
-		}
-		c.Data(http.StatusOK, "image/jpeg", body)
-	})
-
-	// API端点：获取歌单
-	router.GET("/api/playlist", func(c *gin.Context) {
-		filePath := filepath.Join("public", "2024song", "playlist.json")
-		data, err := ioutil.ReadFile(filePath)
-		if err != nil {
-			c.String(http.StatusNotFound, "File not found")
-			return
-		}
-		c.Data(http.StatusOK, "application/json", data)
-	})
-
-	// API端点：获取flac
-// API端点：获取flac或mp3
-router.GET("/api/flac", func(c *gin.Context) {
-    songId := c.Query("songId")
-    jsonPath := filepath.Join("public", "2024song", "playlist.json")
-
-    data, err := ioutil.ReadFile(jsonPath)
-    if err != nil {
-        c.String(http.StatusNotFound, "File not found")
-        return
-    }
-
-    var songs []map[string]interface{}
-    if err := json.Unmarshal(data, &songs); err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
-        return
-    }
-
-    for _, song := range songs {
-        if fmt.Sprint(song["id"]) == songId {
-            songName, ok := song["name"].(string)
-            if !ok {
-                c.String(http.StatusInternalServerError, "Error processing song data")
-                return
-            }
-            flacPath := filepath.Join("public", "2024song", "music", songName+".flac")
-            mp3Path := filepath.Join("public", "2024song", "music", songName+".mp3")
-
-            if _, err := os.Stat(flacPath); err == nil {
-                c.File(flacPath)
-                return
-            } else if _, err := os.Stat(mp3Path); err == nil {
-                c.File(mp3Path)
-                return
-            }
-        }
-    }
-
-    // If no valid song is found
-    c.String(http.StatusNotFound, "Song not found")
-})
-
-
-	// 启动服务器
-	router.Run(":4000")
-}
-
-func fetch(url string) ([]byte, error) {
-	statusCode, body, err := fasthttp.Get(nil, url)
-	if err != nil {
-		return nil, err
-	}
-	if statusCode != http.StatusOK {
-		return nil, fmt.Errorf("server returned non-200 status: %d", statusCode)
-	}
-	return body, nil
-}
 
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -163,3 +20,127 @@ func corsMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func fetch(url string) ([]byte, error) {
+	statusCode, body, err := fasthttp.Get(nil, url)
+	if err != nil {
+		return nil, err
+	}
+	if statusCode != http.StatusOK {
+		return nil, fmt.Errorf("%d", statusCode)
+	}
+	return body, nil
+}
+
+func main(){
+	server:=gin.Default()
+
+	//跨域问题
+	server.Use(corsMiddleware())
+
+	publicGroup:=server.Group("/public")
+	{
+		publicGroup.GET("/2025md/:mdname",func(c *gin.Context){
+			mdname:=c.Param("mdname")
+			mdpath:=filepath.Join("public","2025md",mdname)
+			data,err:=os.ReadFile(mdpath)
+			if err != nil{
+				c.AbortWithStatus(http.StatusNoContent)
+				return
+			}
+			c.Data(http.StatusOK,"text/plain",data)
+		})
+
+		publicGroup.GET("/2025pic/:picname",func(c *gin.Context){
+			picname:=c.Param("picname")
+			picpath:=filepath.Join("public","2025pic",picname)
+			data,err:=os.ReadFile(picpath)
+			if err != nil{
+				c.AbortWithStatus(http.StatusNoContent)
+				return
+			}
+			c.Data(http.StatusOK,"image/jpeg", data)
+		})
+	}
+
+	apiGroup:=server.Group("/api")
+	{
+		apiGroup.GET("/playlist",func(c *gin.Context) {
+			filePath := filepath.Join("public", "2024song", "playlist.json")
+			data, err := os.ReadFile(filePath)
+			if err != nil {
+				c.String(http.StatusNotFound, "Playlist not found")
+				return
+			}
+			c.Data(http.StatusOK, "application/json", data)
+		})		
+		apiGroup.GET("/songInfo", func(c *gin.Context) {
+			songId := c.Query("songId")
+			songUrl := fmt.Sprintf("https://music.163.com/api/v3/song/detail?c=[{\"id\":%s}]", songId)
+			body, err := fetch(songUrl)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+				return
+			}
+			c.Data(http.StatusOK, "application/json", body)
+		})
+		apiGroup.GET("/lyricsInfo", func(c *gin.Context) {
+			songId := c.Query("songId")
+			lyricsUrl := fmt.Sprintf("https://music.163.com/api/song/media?id=%s", songId)
+			body, err := fetch(lyricsUrl)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+				return
+			}
+			c.Data(http.StatusOK, "application/json", body)
+		})
+		apiGroup.GET("/picInfo", func(c *gin.Context) {
+			url := c.Query("picUrl")
+			body, err := fetch(url)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+				return
+			}
+			c.Data(http.StatusOK, "image/jpeg", body)
+		})
+		apiGroup.GET("/flac", func(c *gin.Context) {
+			songId := c.Query("songId")
+			jsonPath := filepath.Join("public", "2024song", "playlist.json")
+			data, _ := os.ReadFile(jsonPath)
+
+			var songs []map[string]interface{}
+			if err := json.Unmarshal(data, &songs); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+				return
+			}
+
+			for _, song := range songs {
+				if fmt.Sprint(song["id"]) == songId {
+					songName, ok := song["name"].(string)
+					if !ok {
+						c.String(http.StatusInternalServerError, "Error processing song data")
+						return
+					}
+
+					flacPath := filepath.Join("public", "2024song", "music", songName+".flac")
+					mp3Path := filepath.Join("public", "2024song", "music", songName+".mp3")
+
+					if _, err := os.Stat(mp3Path); err == nil {
+						c.File(mp3Path)
+						return
+					} else if _, err := os.Stat(flacPath); err == nil {
+						c.File(flacPath)
+						return
+					}
+				}
+			}
+
+			c.String(http.StatusNotFound, "Song not found")
+		})
+
+
+	}
+
+	server.Run(":4000")
+}
+
